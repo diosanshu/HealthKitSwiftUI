35bebe49c3cefabae3893bec7bf37a3dd7f24ecb
diosanshu <diosanshu@gmail.com>
2023-12-20 15:50:36 +0530

    Xcode updated

---
+++ HealthKITSwift/HealthKITSwift/HealthKITSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKITSwiftApp.swift
+//  HealthKITSwift
+//
+//  Created by Haadhya on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthKITSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
